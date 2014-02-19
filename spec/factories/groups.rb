dfa8ef00ff41d0a32cdb7f9c12f5d138b589608f
Javier Ibarrola <ibarroladt@gmail.com>
2014-02-19 11:25:47 -0600

    Tried to fix spec date problem did not work

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -3,8 +3,8 @@ FactoryGirl.define do
     name "Primer Clase"
     cost "280"
     user { FactoryGirl.create(:user) }
-    init_date "10/01/1995"
-    finish_date "20/01/1995"
+    init_date "10/01/2014"
+    finish_date "20/01/2014"
     min_age 0
     max_age 120
     location "Aula uno"
